7761fb2b92e3bea52dc8f89ed2dcde1dfb3a7ec9
mango <Yuzhen0915@gmail.com>
2016-09-22 22:46:12 +0800

    Initial Commit

---
+++ 继承/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  继承
+//
+//  Created by 王宇珍 on 16/9/22.
+//  Copyright (c) 2016年 王宇珍. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
