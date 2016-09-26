8eba22d73eb098d410f4e00da0e2527a47450338
mango <Yuzhen0915@gmail.com>
2016-09-26 11:36:22 +0800

    Initial Commit

---
+++ 常用结构/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  常用结构
+//
+//  Created by 王宇珍 on 16/9/26.
+//  Copyright (c) 2016年 王宇珍. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
