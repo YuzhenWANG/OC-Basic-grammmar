c8b17738cfefd123edc5eca089421b7705ba3df3
mango <Yuzhen0915@gmail.com>
2016-09-26 21:27:02 +0800

    Initial Commit

---
+++ 字符串/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  字符串
+//
+//  Created by 王宇珍 on 16/9/26.
+//  Copyright (c) 2016年 王宇珍. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
